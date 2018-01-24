2dc7f887ca1f8a4741435c60a64191235bbd2ebc
Maik Switalski <Maik.Switalski@gmx.de>
2018-01-24 14:10:32 +0100

    added CRUD-test.

--- Tests/JsonPersistingListTests.cs
+++ Tests/JsonPersistingListTests.cs
@@ -8,23 +8,32 @@ namespace PerpetualEngine.Storage
     [TestFixture]
     public class JsonPersistingListTests
     {
-        string editGroup;
+        const string key = "strings";
+
+        string CollectionString {
+            get { return string.Join(" ", new JsonPersistingList<TestItem>(key)); }
+        }
 
         [SetUp]
         public void Setup()
         {
-            editGroup = Guid.NewGuid().ToString();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            new JsonPersistingList<TestItem>(key).Clear();
         }
 
         [Test]
         public void TestStoringAndLoading()
         {
-            var list = new JsonPersistingList<TestItem>(editGroup);
+            var list = new JsonPersistingList<TestItem>(key);
             list.Add(new TestItem("A"));
-            Assert.True(SimpleStorage.EditGroup(editGroup).HasKey("A"));
-            Assert.AreEqual("{\"Id\":\"A\",\"Value\":\"a\"}", SimpleStorage.EditGroup(editGroup).Get("A"));
+            Assert.True(SimpleStorage.EditGroup(key).HasKey("A"));
+            Assert.AreEqual("{\"Id\":\"A\",\"Value\":\"a\"}", SimpleStorage.EditGroup(key).Get("A"));
 
-            list = new JsonPersistingList<TestItem>(editGroup);
+            list = new JsonPersistingList<TestItem>(key);
             Assert.AreEqual(1, list.Count);
             Assert.AreEqual("A", list.First().Id);
         }
@@ -32,15 +41,15 @@ namespace PerpetualEngine.Storage
         [Test]
         public void TestSkippingNonDeserializableEntries()
         {
-            var list = new JsonPersistingList<TestItem>(editGroup);
+            var list = new JsonPersistingList<TestItem>(key);
             list.Add(new TestItem("1"));
             list.Add(new TestItem("2"));
             list.Add(new TestItem("3"));
 
-            var storage = SimpleStorage.EditGroup(editGroup);
+            var storage = SimpleStorage.EditGroup(key);
             storage.Put("2", "some bad data");
 
-            list = new JsonPersistingList<TestItem>(editGroup);
+            list = new JsonPersistingList<TestItem>(key);
 
             Assert.AreEqual(2, list.Count);
             Assert.AreEqual("1", list.First().Id);
@@ -50,7 +59,7 @@ namespace PerpetualEngine.Storage
         [Test]
         public void AddAllTest()
         {
-            var list = new JsonPersistingList<TestItem>(editGroup);
+            var list = new JsonPersistingList<TestItem>(key);
             int addedCalledCount = 0;
             list.Added += delegate {
                 addedCalledCount++;
@@ -72,6 +81,42 @@ namespace PerpetualEngine.Storage
             Assert.That(addedCalledCount, Is.EqualTo(4));
             Assert.That(list[2].Id, Is.EqualTo("2"));
             Assert.That(list[3].Id, Is.EqualTo("3"));
+        }
+
+        [Test]
+        public void CRUDTests()
+        {
+            var collection = new JsonPersistingList<TestItem>(key) {
+                new TestItem("A"),
+                new TestItem("B"),
+                new TestItem("C"),
+            };
+            Assert.That(CollectionString, Is.EqualTo("Aa Bb Cc"));
+
+            collection.Add(new TestItem("D"));
+            Assert.That(CollectionString, Is.EqualTo("Aa Bb Cc Dd"));
+
+            collection.Insert(2, new TestItem("E"));
+            Assert.That(CollectionString, Is.EqualTo("Aa Bb Ee Cc Dd"));
+
+            collection.Remove("C");
+            Assert.That(CollectionString, Is.EqualTo("Aa Bb Ee Dd"));
+
+            Assert.Throws(typeof(ArgumentOutOfRangeException), () => collection.Remove("C"));
+
+            collection.Update(new TestItem("E", "q"));
+            Assert.That(CollectionString, Is.EqualTo("Aa Bb Eq Dd"));
+
+            collection.Add(new List<TestItem> { new TestItem("X"), new TestItem("Y"), new TestItem("Z") });
+            Assert.That(CollectionString, Is.EqualTo("Aa Bb Eq Dd Xx Yy Zz"));
+
+            collection.Add(new TestItem("x"), new TestItem("y"), new TestItem("z"));
+            Assert.That(CollectionString, Is.EqualTo("Aa Bb Eq Dd Xx Yy Zz xx yy zz"));
+
+            Assert.Throws(typeof(ApplicationException), () => collection.Add(new TestItem("X", "1"), new TestItem("W", "2")));
+
+            collection.Clear();
+            Assert.That(CollectionString, Is.EqualTo(""));
         }
     }
 }
