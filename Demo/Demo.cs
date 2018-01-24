6709934b536a35a198fdb36bcdf1c73a2333262a
Maik Switalski <Maik.Switalski@gmx.de>
2018-01-24 11:32:56 +0100

    added DemoProject (e.g. for performance measurements)

---
+++ Demo/Demo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Demo
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            var content = new ContentPage {
+                Title = "Demo",
+                Content = new StackLayout {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms!"
+                        }
+                    }
+                }
+            };
+
+            MainPage = new NavigationPage(content);
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
